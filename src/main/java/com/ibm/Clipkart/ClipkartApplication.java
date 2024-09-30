bdbd1cd9e1efcc22d83846cad675c257f01ff7e4
Simran757 <simrankashyap757@gmail.com>
2024-09-30 11:29:06 +0530

    Flipkart clone started.

---
+++ src/main/java/com/ibm/Clipkart/ClipkartApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.Clipkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClipkartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClipkartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibm/Clipkart/ClipkartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.Clipkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClipkartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
